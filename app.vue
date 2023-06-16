78e0f96e3d1793ecbf30f9d6158fa47205fa5b14
Piyush <bhangalepiyush@gmail.com>
2023-06-17 00:10:18 +0530

    feat: listen on firebase authentication state change and login to realm using custom JWT token

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
 <script lang="ts" setup>
 import '@unocss/reset/tailwind.css'
+import { getAuth } from 'firebase/auth'
+import { Credentials } from 'realm-web'
+import app from './utils/firebase'
+
+const realmApp = useRealmApp()
+
+consola.info('Initializing Firebase!', app)
+
+onMounted(() => {
+  const auth = getAuth()
+
+  auth.onAuthStateChanged(async (user) => {
+    if (user) {
+      const userIdToken = await user.getIdToken()
+      const credentials = Credentials.jwt(userIdToken)
+
+      try {
+        const realmUser = await realmApp.logIn(credentials)
+        if (realmUser) {
+          // TODO: get notification permission and call atlas function to store notification token
+        }
+
+        navigateTo('/questions')
+      }
+      catch (error: any) {
+        consola.error('[REALM] Failed to log in', error)
+      }
+    }
+  })
+})
 </script>
 
 <template>
