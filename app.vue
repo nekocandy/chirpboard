f71f20cf4b73a9a090464eb80b8e463909674279
Piyush <bhangalepiyush@gmail.com>
2023-06-17 00:55:48 +0530

    feat: login firebase user to realm using custom jwt auth

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import '@unocss/reset/tailwind.css'
 import { getAuth } from 'firebase/auth'
+import { Credentials } from 'realm-web'
 import app from './utils/firebase'
 
 const realmApp = useRealmApp()
@@ -13,23 +14,23 @@ onMounted(() => {
   auth.onAuthStateChanged(async (user) => {
     if (user) {
       const userIdToken = await user.getIdToken()
-      // const credentials = Credentials.jwt(userIdToken)
+      const credentials = Credentials.jwt(userIdToken)
 
-      // try {
-      //   const realmUser = await realmApp.logIn(credentials)
-      //   if (realmUser) {
-      //     const config = useRuntimeConfig()
-      //     const { vapidKey } = config.public
-      //     const token = await getMessageToken(vapidKey)
-      //     if (token)
-      //       await realmUser.functions.callFunction('updateNotificationToken', token)
-      //   }
+      try {
+        const realmUser = await realmApp.logIn(credentials)
+        if (realmUser) {
+          const config = useRuntimeConfig()
+          const { vapidKey } = config.public
+          const token = await getMessageToken(vapidKey)
+          if (token)
+            await realmUser.functions.callFunction('updateNotificationToken', token)
+        }
 
-      //   navigateTo('/questions')
-      // }
-      // catch (error: any) {
-      //   consola.error('[REALM] Failed to log in', error)
-      // }
+        navigateTo('/questions')
+      }
+      catch (error: any) {
+        consola.error('[REALM] Failed to log in', error)
+      }
     }
   })
 })
