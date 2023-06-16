22b88c369e010831861c74113efb8cd9ced70530
Piyush <bhangalepiyush@gmail.com>
2023-06-16 23:19:29 +0530

    feat: add a login layout

---
+++ layouts/login.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div bg-zinc-900 font-mono>
+    <div h-screen flex items-center justify-center text-white>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
