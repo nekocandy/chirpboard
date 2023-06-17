b636ec05b5d86c9f07c7ced7b7f1c1b40512675a
Piyush <bhangalepiyush@gmail.com>
2023-06-17 13:42:31 +0530

    feat: use background image for questions page

--- pages/questions.vue
+++ pages/questions.vue
@@ -82,31 +82,33 @@ watch(isApproved, (newValue) => {
 </script>
 
 <template>
-  <div relative flex flex-col justify-center gap-4 rounded-md bg-zinc-950 p-8>
-    <div absolute bottom-0 left-0 right-0 top-0 z-10 items-center justify-center rounded-md bg-zinc-950 :class="{ flex: isLoading, hidden: !isLoading }">
-      <div class="h-8 w-8 text-white" i-line-md-loading-twotone-loop />
-    </div>
-    <!-- user avatar and info -->
-    <div class="flex">
-      <div flex items-center gap-4>
-        <img class="h-12 w-12 rounded-full" :src="userImage" :alt="`${userName}'s Profile'`">
-        <div>{{ userName }}</div>
+  <div h-full w-full flex items-center justify-center bg-cover bg-center bg-no-repeat class="bg-[url(/bg.gif)]">
+    <div relative flex flex-col items-center justify-center gap-4 rounded-md bg-zinc-950 p-8 class="w-1/3 bg-opacity-85">
+      <div absolute bottom-0 left-0 right-0 top-0 z-10 items-center justify-center rounded-md bg-zinc-950 :class="{ flex: isLoading, hidden: !isLoading }">
+        <div class="h-8 w-8 text-white" i-line-md-loading-twotone-loop />
       </div>
-    </div>
-
-    <!-- questions if not answered -->
-    <div v-if="!answeredQuestions" flex flex-col gap-3>
-      <textarea v-model="whyWantToJoin" placeholder="Why do you want to join the platform?" h-24 w-45vh resize-none border-2 border-zinc-600 rounded-md bg-zinc-900 px-2 py-2 focus:outline-none />
-
-      <button class="relative rounded-full bg-teal-600 px-4 py-2 text-center" @click="submitAnswers">
-        <div v-if="isSubmitting" absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center rounded-full bg-teal-600>
-          <div class="h-8 w-8 text-white" i-line-md-loading-twotone-loop />
+      <!-- user avatar and info -->
+      <div class="flex">
+        <div flex items-center gap-4>
+          <img class="h-12 w-12 rounded-full" :src="userImage" :alt="`${userName}'s Profile'`">
+          <div>{{ userName }}</div>
         </div>
-        Submit!
-      </button>
-    </div>
-    <div v-else relative>
-      Approval Status: <span class="font-bold"> {{ isApproved === null ? 'Under Review' : (isApproved ? 'Approved' : 'Denied') }}</span>
+      </div>
+
+      <!-- questions if not answered -->
+      <div v-if="!answeredQuestions" flex flex-col gap-3>
+        <textarea v-model="whyWantToJoin" placeholder="Why do you want to join the platform?" h-24 w-45vh resize-none border-2 border-zinc-600 rounded-md bg-zinc-900 px-2 py-2 focus:outline-none />
+
+        <button class="relative rounded-full bg-teal-600 px-4 py-2 text-center" @click="submitAnswers">
+          <div v-if="isSubmitting" absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center rounded-full bg-teal-600>
+            <div class="h-8 w-8 text-white" i-line-md-loading-twotone-loop />
+          </div>
+          Submit!
+        </button>
+      </div>
+      <div v-else relative>
+        Approval Status: <span class="font-bold"> {{ isApproved === null ? 'Under Review' : (isApproved ? 'Approved' : 'Denied') }}</span>
+      </div>
     </div>
   </div>
 </template>
