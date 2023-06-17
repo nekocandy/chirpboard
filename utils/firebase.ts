3471e71e077442a00cc76aceef55cdac03501641
Piyush <bhangalepiyush@gmail.com>
2023-06-17 10:25:02 +0530

    feat(utils): add FCM to firebase

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,5 +1,7 @@
 import type { FirebaseApp } from 'firebase/app'
 import { getApp, getApps, initializeApp } from 'firebase/app'
+import { getMessaging, getToken, onMessage } from 'firebase/messaging'
+import consola from './consola'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAl9OBBZtFsxzX0A79XY5YhDzdRKlG2Iew',
@@ -18,5 +20,29 @@ if (getApps().length)
 
 else
   app = initializeApp(firebaseConfig)
+
+const messaging = getMessaging()
+
+onMessage(messaging, (payload) => {
+  const { title, ...options } = payload.notification!
+
+  // eslint-disable-next-line no-new
+  new Notification(title || 'No title', options)
+})
+
+export async function getMessageToken(vapidKey: string) {
+  try {
+    await Notification.requestPermission()
+    const token = getToken(messaging, {
+      vapidKey,
+    })
+
+    return token
+  }
+  catch (error) {
+    consola.error('Notification permission denied', error)
+    return null
+  }
+}
 
 export default app
